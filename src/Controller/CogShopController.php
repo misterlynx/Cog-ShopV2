279a04db76eb74d40d4aeec0aa8c761527af9d0f
KG42 <gergelovakarla@gmail.com>
2020-02-12 16:52:03 +0100

    front changes

--- src/Controller/CogShopController.php
+++ src/Controller/CogShopController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UsersRepository;
 use Symfony\Component\HttpFoundation\Request;
+use Knp\Bundle\PaginatorBundle\KnpPaginatorBundle;
 
 class CogShopController extends AbstractController
 {
